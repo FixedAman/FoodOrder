f0797a8849803e722e37e2114496acc9214dbef4
Suvasis76 <suvasisjanacool1000@gmail.com>
2025-02-21 15:30:21 +0530

    making_footer

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,4 +1,9 @@
 import { FaFacebook, FaInstagram, FaTwitter, FaPhone, FaEnvelope } from "react-icons/fa";
+import { useRef, useState } from "react";
+
+const Header = () => {
+  
+  };
 
 const Footer = () => {
   return (
@@ -13,31 +18,23 @@ const Footer = () => {
           <p className="text-sm">Delicious food at your doorstep</p>
         </div>
 
-        {/* Navigation Links */}
-        <div className="flex flex-wrap justify-center space-x-6">
-          <a href="#" className="hover:text-gray-300 transition">Home</a>
-          <a href="#" className="hover:text-gray-300 transition">Menu</a>
-          <a href="#" className="hover:text-gray-300 transition">About Us</a>
-          <a href="#" className="hover:text-gray-300 transition">Contact</a>
-        </div>
+        {/* Copyright Section */}
+        <div className="text-center text-sm mt-6 border-b border-white/20 pt-4">
+        © {new Date().getFullYear()} Foodie. All Rights Reserved.
+      </div>
 
         {/* Social Icons */}
         <div className="flex space-x-4 mt-4 md:mt-0">
-          <a href="#" className="text-2xl hover:text-gray-300 transition">
+          <a href="https://www.facebook.com/" className="text-2xl hover:text-gray-300 transition">
             <FaFacebook />
           </a>
-          <a href="#" className="text-2xl hover:text-gray-300 transition">
+          <a href="https://www.instagram.com/accounts/login/?hl=en" className="text-2xl hover:text-gray-300 transition">
             <FaInstagram />
           </a>
-          <a href="#" className="text-2xl hover:text-gray-300 transition">
+          <a href="https://x.com/i/flow/login" className="text-2xl hover:text-gray-300 transition">
             <FaTwitter />
           </a>
         </div>
-      </div>
-
-      {/* Copyright Section */}
-      <div className="text-center text-sm mt-6 border-t border-white/20 pt-4">
-        © {new Date().getFullYear()} Foodie. All Rights Reserved.
       </div>
     </footer>
   );
